9b882b493a586ee4436f78b4609546ab7178be72
Witherflare <marcelo.goluboff@gmail.com>
2023-12-17 13:07:17 -0500

    first commit

---
+++ src/main/java/com/circuitwizardry/missioncontrol/Missioncontrol.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.circuitwizardry.missioncontrol;
+
+/**
+ *
+ * @author marce
+ */
+public class Missioncontrol {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        var frame = new MainScreen();
+        frame.setVisible(true);
+    }
+}
---
+++ src/main/java/com/circuitwizardry/missioncontrol/features/Feature.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.circuitwizardry.missioncontrol.features;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author marce
+ */
+public class Feature extends JPanel {
+    public String generateJson() {
+        return "No JSON specified.";
+    }
+}
